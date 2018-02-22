9986e7578adc0444e4f4ae4ed2c7530ed5e22adc
hunter <hantek.f@gmail.com>
2018-02-22 05:20:17 +0100

    change app.py

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 import cherrypy
-import os
-from jinja2 import Environment, FileSystemLoader
+import simplejson
 
-env = Environment(loader=FileSystemLoader('html'))
+class Root(object):
 
-class HelloWorld(object):
+    @cherrypy.expose
+    def update(self):
+        cl = cherrypy.request.headers['Content-Length']
+        rawbody = cherrypy.request.body.read(int(cl))
+        body = simplejson.loads(rawbody)
+        # do_something_with(body)
+        return "Updated %r." % (body,)
+
     @cherrypy.expose
     def index(self):
-        tmpl = env.get_template('index.html')
-        return tmpl.render()
-
-    @cherrypy.expose
-    def paid(self):
-        tmpl = env.get_template('paid.html')
-        return tmpl.render()
-
-    @cherrypy.expose
-    def seller(self):
-        tmpl = env.get_template('seller.html')
-        return tmpl.render()
-
-config = {
-    'global': {
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': int(os.environ.get('PORT', 5000)),
-    },
-    '/assets': {
-        'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': 'assets',
-    }
+        return """
+<html>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type='text/javascript'>
+function Update() {
+    $.ajax({
+      type: 'POST',
+      url: "update",
+      contentType: "application/json",
+      processData: false,
+      data: $('#updatebox').val(),
+      success: function(data) {alert(data);},
+      dataType: "text"
+    });
 }
+</script>
+<body>
+<input type='textbox' id='updatebox' value='{}' size='20' />
+<input type='submit' value='Update' onClick='Update(); return false' />
+</body>
+</html>
+"""
 
-cherrypy.quickstart(HelloWorld(), '/', config=config)
+cherrypy.quickstart(Root())
--- assets/scripts.js
+++ assets/scripts.js
@@ -12,3 +12,23 @@ $(document).ready(function(){
     });
     
 });
+
+setInterval(function(){
+    get_post()         
+},5000);
+
+function get_post(){
+    var Url = 'https://ancient-inlet-30790.herokuapp.com/seller'
+    $.ajax({
+        url: Url,
+        type: 'POST',
+        dataType: 'json',
+        data: {},
+    })
+    .done(function(data) {
+        //here assign the returned data to the html element
+    })
+    .fail(function() {
+        console.log("error");
+    });
+}
